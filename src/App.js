607437e97f850d70da8ac1a2463ce32986c889a6
Sergey Sharov <lirnik88@mail.ru>
2025-04-09 15:12:30 +0400

    добавил файловую структуру для работы над проектом

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>Thats all</p>
+        </div>
+    );
+}
+
+export default App;
