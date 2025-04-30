1517795347e60ee6cd9b87174609602698f58d37
Sergey Sharov <lirnik88@mail.ru>
2025-04-30 09:44:35 +0400

    добавлен файл api.js, библиотека Redux, компонент Catalog и стили

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,10 +1,113 @@
 
 import './style.css';
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { increaseGoods } from '../../store/goodsCounterSlice';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { getApiData } from './../../services/api';
+
+const violetBase = '#802580';
+
+const theme = createTheme({
+  palette: {
+    violet: {
+      main: violetBase,
+    },
+  },
+});
 
 function Catalog() {
+    const [goodsList, setGoodsList] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const [searchThing, setSearchThing] = useState('');
+    const dispatch = useDispatch();
+    const logIn = localStorage.getItem('logIn');
+
+    useEffect(() => {
+        const loadCatalog = async () => {
+            try {
+                setLoading(true);
+                const goods = await getApiData();
+                setGoodsList(goods);
+                setLoading(false);
+            } catch (error) {
+                console.log(error.name + ': ' + error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadCatalog();
+    }, []);
+
+    const addToCart = () => {
+        if (logIn !== null) {
+            dispatch(increaseGoods());
+        } else {
+            alert('To make a purchase, please log in');
+        }
+    }
+
+    const filteredGoods = useMemo(() => {
+        return goodsList.filter(thing => {
+            return thing.title.toLowerCase().includes(searchThing.toLowerCase());
+        })
+    }, [goodsList, searchThing]);
+
+
+    const catalog = filteredGoods.map(({id, title, price, description, category, image}) => {
+        return <Card className="card" key={id} 
+                sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={image}
+                        title={title}
+                />
+                    <ThemeProvider theme={theme}>
+                        <CardContent className="card__description">
+                            <Typography gutterBottom variant="h6" component="div">
+                                {title}
+                            </Typography>
+                            <Typography gutterBottom variant="h6" component="div" color="violet">
+                                Price: {price}$
+                            </Typography>
+                            <Typography gutterBottom component="div">
+                                #{category}
+                            </Typography>
+
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                {description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions className="card__buttons">
+                            <Button size="small" color="violet"
+                             onClick={addToCart}>
+                                BUY <ShoppingCartIcon />
+                            </Button>
+
+                            <Button size="small" color="violet">
+                                LIKE <FavoriteBorderIcon />
+                            </Button>
+                        </CardActions>
+                    </ThemeProvider>
+                </Card>
+    }); 
+
     return (
-        <></>
+        <div className="catalog wrapper">
+            <input className="catalog__search"
+             onChange={e => setSearchThing(e.target.value)}
+             value={searchThing}
+             placeholder="enter the name of thing"
+            />
+
+            <div className='catalog__info'>
+                {catalog}
+            </div>
+        </div> 
     );
 }
 
