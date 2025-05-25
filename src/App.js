c96c08e09ba93623818ec1167dd64663b5905586
Sergey Sharov <lirnik88@mail.ru>
2025-05-25 17:14:12 +0400

    добавление компонента Orders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './pages/Header/';
-import { Catalog } from './pages/Catalog/';
-import { About } from './pages/About/';
-import { Delivery } from './pages/Delivery/';
-import { Main } from './pages/Main/';
-import { Footer } from './pages/Footer/';
-import { Registration } from './pages/Registration/';
-import { Cart } from './pages/Cart/';
+import { Header } from './pages/Header';
+import { Catalog } from './pages/Catalog';
+import { About } from './pages/About';
+import { Delivery } from './pages/Delivery';
+import { Main } from './pages/Main';
+import { Footer } from './pages/Footer';
+import { Registration } from './pages/Registration';
+import { Cart } from './pages/Cart';
+import { Orders } from './pages/Orders';
 import { ThemeProvider } from './services/ThemeProvider';
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
                             <Route path="/delivery" element={ <Delivery /> }/>
                             <Route path="/registration" element={ <Registration /> }/>
                             <Route path="/cart" element={ <Cart /> }/>
+                            <Route path="/orders" element={ <Orders /> }/>
                         </Routes>
                     <Footer />
                 </Router>
